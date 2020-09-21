5cfc7cf5c22ecae03b634fa3aa7912e47ba52d5f
Rafael Hipólito <64715008+RafaelHipolit@users.noreply.github.com>
2020-09-20 23:51:25 -0300

    PHP adicionado
    Nav logar no Index
    login.php

---
+++ php/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login_css.css">
+    <title>Login - projectSubarashii</title>
+</head>
+<body>
+    <div id="corpo">
+    
+    <div id="divLogin">
+        <div id="divForm">
+
+            <p id="title">MyPage Project 素晴らしい</p>
+            
+            <form action="myPage.php">
+                
+                <div class="input">
+                    <label for="txtUser" >Usuario: </label>
+                    <input type="text" name="User" id="txtUser" size="30" maxlength="150">
+                </div>
+                
+                <div class="input">
+                    <label for="txtSenha" >Senha: </label>
+                    <input type="text" name="Senha" id="txtSenha" size="34" maxlength="50">
+                </div>
+                      
+                <input type="submit" name="btnEntrar" value="Entrar" id="btnEntrar">
+                
+            </form>
+
+        </div>
+    </div>
+    
+    </div>
+</body>
+</html>
+
